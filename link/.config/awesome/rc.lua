27c612f1454019ec4a2c64f01809054607ddfca0
Pieter Frenssen <pieter@frenssen.be>
2013-08-18 11:10:04 +0200

    Default to vim if no editor is specified in the environment variables.

--- link/.config/awesome/rc.lua
+++ link/.config/awesome/rc.lua
@@ -42,7 +42,7 @@ beautiful.init(awful.util.getdir("config") .. "/theme/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
-editor = os.getenv("EDITOR") or "nano"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
