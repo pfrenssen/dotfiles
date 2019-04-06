98faaae9569cb444712e9ec1b8a9965fadbd812c
Pieter Frenssen <pieter@frenssen.be>
2019-04-06 10:20:16 +0200

    Switch to Alacritty as default editor in AwesomeWM.

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -42,7 +42,7 @@ end
 beautiful.init(awful.util.getdir("config") .. "/theme/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "alacritty"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
